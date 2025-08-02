7dd3d026e656b69891b9e2d090d4b369e17b8e21
ankitha458 <ankithamk345@gmail.com>
2025-08-02 10:56:38 +0530

    Add files via upload

---
+++ Switch1.java
@@ -0,0 +1,28 @@
+public class Switch1 {
+	public static void main(String args[])
+	{
+		char alphabet = 'i';                                    
+	    switch (alphabet)
+	    {
+	    case  'a':
+	    	System.out.println("a is vowel");
+	    	break;
+	    case 'e':
+	    	System.out.println("e is vowel");
+	    	break;
+	    case 'i':
+	    	System.out.println("i is vowel");
+	    	break;
+	    case 'o':
+	    	System.out.println("o is vowel");
+	    	break;
+	    case 'u':
+	    	System.out.println("u is vowel");
+	    	break;
+	    default:
+	    	System.out.println(" is not a vowel");
+	    	
+	    }
+	    }
+
+}
